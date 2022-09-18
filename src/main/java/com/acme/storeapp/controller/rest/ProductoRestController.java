139d13407ef6733d4a29118412d4c53ce636d15f
diegocho <diegoalonzo2001@hotmail.com>
2022-09-18 02:08:48 -0500

    appfinal

--- src/main/java/com/acme/storeapp/controller/rest/ProductoRestController.java
+++ src/main/java/com/acme/storeapp/controller/rest/ProductoRestController.java
@@ -16,9 +16,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 import java.util.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "api/productos", produces = "application/json")
 public class ProductoRestController {
     private final ProductoRepository productsData;
@@ -31,5 +34,39 @@ public class ProductoRestController {
     public ResponseEntity<List<Producto>> productos(){
         return  new ResponseEntity<List<Producto>>(
             productsData.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Integer> create(@RequestBody Producto e){
+        productsData.save(e);
+        productsData.flush();
+        return new ResponseEntity<Integer>(e.getId(),HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Producto> Productos(@PathVariable int id){
+        Optional<Producto> optinalEntity = productsData.findById(id);
+        if(optinalEntity.isPresent())
+            return new ResponseEntity<Producto>(
+                optinalEntity.get(), HttpStatus.OK);
+        else
+            return new ResponseEntity<Producto>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity delete(@PathVariable int id){
+        productsData.deleteById(id);
+        return new ResponseEntity(HttpStatus.OK);
+    }   
+    
+    @PutMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Producto> update(@RequestBody Producto update){
+        Optional<Producto> optinalEntity = productsData.findById(update.getId());
+        if(optinalEntity.isPresent()){
+            Producto current = optinalEntity.get();
+            current.setDescripcion(update.getDescripcion());
+            create(current);
+        }
+        return new ResponseEntity<Producto>(HttpStatus.OK);
     }
 }
